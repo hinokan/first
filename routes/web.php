d6ef11920ae6ae5d831aec0d6a2a3975f70e86aa
hinokan <723224k@gmail.com>
2024-07-19 14:55:52 +0900

    8-5

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ use App\Http\Controllers\PostController;
 |
 */
 
+Route::get('/posts/{post}/edit',[PostController::class,'edit']);
 Route::get('/', [PostController::class,'index']);
 Route::get('/posts/create', [PostController::class,'create']);
 Route::get('/posts/{post}', [PostController::class,'show']);
 Route::post('/posts', [PostController::class,'store']);
+Route::put('/posts/{post}',[PostController::class,'update']);
 
